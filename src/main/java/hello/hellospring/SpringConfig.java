62413be84d15a8fe4b146b4001fb9c22674b3da4
Son-Gyeongi <thsruddl77@gmail.com>
2023-03-29 17:32:41 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.Repository.JpaMemberRepository;
-import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 /*
 자바 코드로 직접 스프링 빈 등록하기
@@ -16,15 +12,18 @@ import javax.persistence.PersistenceContext;
 @Configuration // @Configuration 스프링 빈으로 관리됨
 public class SpringConfig {
 
+    /*
     // jdbc에 dataSource 파라미터 필요
     // 스프링이 제공해준다.
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
+    private DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+     */
+
+    /*
 //    @PersistenceContext 이렇게도 할 수 있다.
     private EntityManager em;
 
@@ -32,18 +31,27 @@ public class SpringConfig {
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+     */
+
+    // 스프링 데이터 JPA가 만들어놓은 구현체가 등록이 된다.
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // spring Bean으로 등록할 거다
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource); 순수 JDBC
 //        return new JdbcTemplateMemberRepository(dataSource); // JdbcTemplate
-        return new JpaMemberRepository(em); // JPA
-    }
+//        return new JpaMemberRepository(em); // JPA
+//    }
 }
